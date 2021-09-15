d183fcae3170ff8b00c3e451dff5f8c825efc397
Denis Deratani Maua <denis.maua@usp.br>
2021-09-15 13:50:40 +0000

    Update agent.py

--- regression/agent.py
+++ regression/agent.py
@@ -1,4 +1,4 @@
-# Intro to Robotics - MAC0318
+# MAC0318 - Intro to Robotics
 #
 # Name:
 # NUSP:
@@ -6,88 +6,135 @@
 # ---
 #
 # Assignment 8 - Lane following as a regression task
-# Carefully read this header and follow submission instructions!
-# Failure to follow instructions may result in a zero!
-#
-# Don't forget to run this from the Duckievillage root directory!
-# From within the root directory, run python with the -m flag.
-#   python3 -m assignments/regression
+# Don't forget to run this file from the Duckievillage root directory path (example):
+#   cd ~/MAC0318/duckievillage
+#   git pull
+#   $(bash) update.sh
+#   conda activate duckietown
+#   python3 assignments/pid-control/agent.py
 #
 # Submission instructions:
-#  0. Add your name and USP number to the header's header.
-#  1. Make sure everything is running fine and there are no errors during startup. If the code does
-#     not even start the environment, you will receive a zero.
-#  2. Test your code and make sure it's doing what it's supposed to do.
-#  3. Append your NUSP to this file name.
-#  4. Submit your work to e-disciplinas.
+#  0. Add your name and USP number to the file header above.
+#  1. Make sure that any last change hasn't broken your code. If the code crashes without running you'll get a 0.
+#  2. Submit this file via e-disciplinas.
+
 
 import sys
-import math
 import pyglet
 import numpy as np
+import math
 from pyglet.window import key
-import gym
-import gym_duckietown
-from duckievillage import DuckievillageEnv, FRONT_VIEW_MODE
-
+from duckievillage import create_env
 import cv2
 import tensorflow
 import tensorflow.keras as keras
-
-env = DuckievillageEnv(
-  seed = 101,
-  map_name = 'udem1',
-  draw_curve = False,
-  draw_bbox = False,
-  domain_rand = False,
-  distortion = False,
-  top_down = False,
-)
-
-env.set_view(FRONT_VIEW_MODE)
-
-env.reset()
-env.render()
-
-@env.unwrapped.window.event
-def on_key_press(symbol, modifiers):
-  if symbol == key.ESCAPE:
-    env.close()
-    sys.exit(0)
-  elif symbol == key.C:
-    capture()
-  env.render()
-
-key_handler = key.KeyStateHandler()
-env.unwrapped.window.push_handlers(key_handler)
-
-# Loads a Tensorflow model.
-def load_model(filepath):
-  N = keras.models.load_model(filepath)
-  N.summary()
-  return N
 
 # Converts an RGB image to grayscale.
 def rgb2gray(I):
   return np.dot(I[...,:3], [0.2989, 0.5870, 0.1140])
 
-# Preprocess image to fit our trained model.
-def preprocess_image():
-  I = env.front() # captures image from the robot's front camera
-  J = cv2.resize(I, dsize=(200, 150)) # resize it to reduce processing cost
-  return rgb2gray(J).astype(np.uint8)
+class Agent:
+    # Agent initialization
+    def __init__(self, environment):
+        ''' Initializes agent '''
+        self.env = environment
+        # Wheel radius
+        self.radius = 0.0318 # R
+        # Distance between wheels
+        self.baseline = environment.unwrapped.wheel_dist # 2L = 0.102 [m]
+         # Motor constants
+        self.motor_gain = 0.68*0.0784739898632288 # K_m
+        self.motor_trim = 0.0007500911693361842 # K_t
+        # Controller
+        key_handler = key.KeyStateHandler()
+        environment.unwrapped.window.push_handlers(key_handler)
+        self.velocity = 0.0 # robot's logitudinal velocity
+        self.rotation = 0.0 # robot's angular velocity
+        self.key_handler = key_handler
 
-# Implement your control system here. It should return an array of the action to be taken.
-def send_commands(dt):
-  action = 0.0, 0.0
+    def load_regression_model(self, filepath):
+        ''' Loads a Tensorflow model. '''
+        N = keras.models.load_model(filepath)
+        N.summary()
+        return N
+    
+    def preprocess(self) -> float:
+        ''' Returns the metric to be used as signal for the PID controller. '''
+        I = env.front() # captures image from the robot's front camera
+        J = cv2.resize(I, dsize=(200, 150)) # resize it to reduce processing cost
+        J = rgb2gray(J).astype(np.uint8)        
+        return 0.0 # value y=6*d + alpha      
 
-  obs, reward, done, info = env.step(action)
+    def get_pwm_control(self, v: float, w: float)-> (float, float):
+        ''' Takes velocity v and angle w and returns left and right power to motors.'''
+        V_l = (self.motor_gain - self.motor_trim)*(v-w*self.baseline/2)/self.radius
+        V_r = (self.motor_gain + self.motor_trim)*(v+w*self.baseline/2)/self.radius
+        return V_l, V_r
 
-  env.render()
+    def send_commands(self, dt) -> None:
+        ''' Agent control loop '''
+        # Manual control for testing in order to understand the environment.
+        # You should delete this snippet after your controller is set.
+        if self.key_handler[key.W]:
+            self.velocity = 0.2
+        if self.key_handler[key.A]:
+            self.rotation += 0.5
+        if self.key_handler[key.S]:
+            self.velocity = 0.0
+        if self.key_handler[key.D]:
+            self.rotation = -0.5
+        # End of remote control snippet.
 
+        pwm_left, pwm_right = self.get_pwm_control(self.velocity, self.rotation)
 
-pyglet.clock.schedule_interval(update, 1.0 / env.unwrapped.frame_rate)
+        # Target value for lane-following.
+        y = self.preprocess()
+        # print(y) # uncomment this for debugging
 
-pyglet.app.run()
+        self.env.step(pwm_left, pwm_right) # send commands to motor
+        self.env.render() # simulate environment
 
-env.close()
+def main():
+    print("MAC0318 - Assignment 8")
+    env = create_env(
+        raw_motor_input = True,
+        noisy = True,
+        mu_l = 0.007123895,
+        mu_r = -0.000523123,
+        std_l = 1e-7,
+        std_r = 1e-7,
+        seed = 101,
+        map_name = 'udem1',
+        draw_curve = False,
+        draw_bbox = False,
+        domain_rand = False,
+        #user_tile_start = (0, 0),
+        distortion = False,
+        top_down = False,
+        cam_height = 10,
+        #is_external_map = True,
+        randomize_maps_on_reset = False,
+    )
+    env.set_view(FRONT_VIEW_MODE)
+
+    env.reset()
+    env.render('human')
+
+    @env.unwrapped.window.event
+    def on_key_press(symbol, modifiers):
+        if symbol == key.ESCAPE: # exit simulation
+            env.close()
+            sys.exit(0)
+        elif symbol == key.RETURN: # Reset pose.
+            env.reset_pos()
+
+        env.render() # show image to user
+
+    agent = Agent(env)
+    pyglet.clock.schedule_interval(agent.send_commands, 1.0 / env.unwrapped.frame_rate)
+    pyglet.app.run()
+    env.close()
+
+if __name__ == '__main__':
+    main()
+
