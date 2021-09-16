ac37eadb2378edf7cea435d30882826c8599c46b
Renato Lui Geh <renatogeh@gmail.com>
2021-09-16 17:51:52 -0300

    Regression template

--- regression/agent.py
+++ regression/agent.py
@@ -6,6 +6,7 @@
 # ---
 #
 # Assignment 8 - Lane following as a regression task
+#
 # Don't forget to run this file from the Duckievillage root directory path (example):
 #   cd ~/MAC0318/duckievillage
 #   git pull
@@ -18,20 +19,15 @@
 #  1. Make sure that any last change hasn't broken your code. If the code crashes without running you'll get a 0.
 #  2. Submit this file via e-disciplinas.
 
-
 import sys
 import pyglet
 import numpy as np
 import math
 from pyglet.window import key
-from duckievillage import create_env
+from duckievillage import create_env, FRONT_VIEW_MODE
 import cv2
 import tensorflow
 import tensorflow.keras as keras
-
-# Converts an RGB image to grayscale.
-def rgb2gray(I):
-  return np.dot(I[...,:3], [0.2989, 0.5870, 0.1140])
 
 class Agent:
     # Agent initialization
@@ -42,9 +38,19 @@ class Agent:
         self.radius = 0.0318 # R
         # Distance between wheels
         self.baseline = environment.unwrapped.wheel_dist # 2L = 0.102 [m]
-         # Motor constants
+        # Motor constants
         self.motor_gain = 0.68*0.0784739898632288 # K_m
         self.motor_trim = 0.0007500911693361842 # K_t
+        # Filter
+        # Use your Braitenberg filters here!
+        self.inner_lower = np.array([0, 0, 0])
+        self.inner_upper = np.array([0, 0, 0])
+        self.outer_lower = np.array([0, 0, 0])
+        self.outer_upper = np.array([0, 0, 0])
+        # Regressor
+        self.regressor = Agent.load_regression_model("my_super_accurate_regressor.h5")
+        self.mean = 0.0
+        self.std = 0.0
         # Controller
         key_handler = key.KeyStateHandler()
         environment.unwrapped.window.push_handlers(key_handler)
@@ -52,18 +58,28 @@ class Agent:
         self.rotation = 0.0 # robot's angular velocity
         self.key_handler = key_handler
 
-    def load_regression_model(self, filepath):
+    @staticmethod
+    def load_regression_model(filepath: str) -> keras.Model:
         ''' Loads a Tensorflow model. '''
         N = keras.models.load_model(filepath)
         N.summary()
         return N
-    
+
+    def normalize(self, X: np.ndarray) -> np.ndarray:
+        ''' Normalizes an array with mean self.mean and standard deviation self.std. '''
+        return (X-self.mean)/self.std
+
     def preprocess(self) -> float:
         ''' Returns the metric to be used as signal for the PID controller. '''
-        I = env.front() # captures image from the robot's front camera
-        J = cv2.resize(I, dsize=(200, 150)) # resize it to reduce processing cost
-        J = rgb2gray(J).astype(np.uint8)        
-        return 0.0 # value y=6*d + alpha      
+        I = self.env.front() # captures image from the robot's front camera
+        I = cv2.resize(I, (128, 96)) # resize it to reduce processing cost
+        hsv = cv2.cvtColor(I, cv2.COLOR_RGB2HSV)
+        inner_mask = cv2.inRange(hsv, self.inner_lower, self.inner_upper)//255
+        outer_mask = cv2.inRange(hsv, self.outer_lower, self.outer_upper)//255
+        mask = cv2.bitwise_or(inner_mask, outer_mask)
+        I = self.normalize(cv2.bitwise_and(I, I, mask=mask).reshape(1, 128*96*3))
+        y = float(self.regressor(I))
+        return y # value y=6*d + alpha
 
     def get_pwm_control(self, v: float, w: float)-> (float, float):
         ''' Takes velocity v and angle w and returns left and right power to motors.'''
@@ -85,12 +101,11 @@ class Agent:
             self.rotation = -0.5
         # End of remote control snippet.
 
-        pwm_left, pwm_right = self.get_pwm_control(self.velocity, self.rotation)
-
         # Target value for lane-following.
         y = self.preprocess()
         # print(y) # uncomment this for debugging
 
+        pwm_left, pwm_right = self.get_pwm_control(self.velocity, self.rotation)
         self.env.step(pwm_left, pwm_right) # send commands to motor
         self.env.render() # simulate environment
 
@@ -104,15 +119,15 @@ def main():
         std_l = 1e-7,
         std_r = 1e-7,
         seed = 101,
-        map_name = 'udem1',
+        map_name = './maps/minimal_udem1.yaml',
         draw_curve = False,
         draw_bbox = False,
         domain_rand = False,
-        #user_tile_start = (0, 0),
+        user_tile_start = (0, 0),
         distortion = False,
         top_down = False,
         cam_height = 10,
-        #is_external_map = True,
+        is_external_map = True,
         randomize_maps_on_reset = False,
     )
     env.set_view(FRONT_VIEW_MODE)
