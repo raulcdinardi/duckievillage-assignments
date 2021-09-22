5777e2eeb7d0b7d182b6312bfa9d4cd37a47c56c
Renato Lui Geh <renatogeh@gmail.com>
2021-09-22 14:55:01 -0300

    Add regression-cnn

---
+++ regression-cnn/README.md
@@ -0,0 +1,69 @@
+# MAC0318 Introdução à Programação de Robôs Móveis
+
+## Atividade 9 - Seguidor de pista com redes neurais convolucionais
+
+Nessa atividade, você deve implementar um agente seguidor de pista de forma similar ao da
+[atividade anterior](../regression). Na tarefa anterior, você implementou um seguidor de pista
+através de uma rede neural como módulo de percepção, usando dados já coletados préviamente para
+predizer o sinal de controle $`y=C\cdot d+\alpha`$, onde $`d`$ é a distância do robô até a faixa a
+ser seguida e $`\alpha`$ é a diferença entre o ângulo do carrinho e da faixa, a partir de uma
+imagem. O sinal $`y`$ é então repassado ao controlador, que decide a velocidade linear e angular do
+carrinho.
+
+Nesta atividade, ao invés de separarmos o agente em módulo de percepção e controle, iremos usar um
+modelo de rede neural convolucional para predizer as velocidades diretamente da imagem. Para isso,
+precisaremos coletar os dados para predição.
+
+### Coletando dados
+
+Você usará duas classes de agente no arquivo [agent.py](./agent.py): `DataAgent` e
+`EvaluationAgent`. O primeiro será utilizado para coletar os dados necessários para treinar nosso
+modelo, enquanto que o outro avaliará a rede treinada. O agente `DataAgent` percorrerá o circuito
+de forma automática (igual a atividade de [Controle PID](../pid-control)) e ao mesmo tempo
+coletará as imagens vistas pelo robô e as ações/velocidades dadas pelo controlador, que agirão como
+rótulos.
+
+A princípio, `DataAgent` vem apenas com um controlador proporcional simples. Isso quer dizer que as
+predições do nosso eventual modelo terão como base o comportamento de um controlador simples, que é
+altamente oscilatório nas curvas. Para que nosso modelo prediga as trajetórias de forma mais suave,
+precisamos usar um controlador PID. Use o seu controlador PID da atividade de
+[Controle PID](../pid-control) para gerar os dados.
+
+**Observação:** é importante que os dados contenham poses variadas, e não apenas aquelas vistas
+pelo controlador em seu trajeto "ideal". Você pode manualmente guiar o robô para poses que o agente
+não veria normalmente, mas não se esqueça de gravar apenas as ações que o controlador tomaria, e
+não as ações manuais que levam o robô para fora do ideal.
+
+O método `DataAgent.send_commands` redimensiona as imagens para $`80\times 60`$, rotulando cada
+imagem com a velocidade linear e angular. A função `on_key_press` salva as imagens e rótulos com o
+pressionar do botão `ESC`.
+
+### Treinando seu modelo
+
+Agora que temos os dados, podemos aprender nosso modelo. Treine uma rede neural convolucional que
+toma como entrada imagens coloridas $`80x60`$ e retorna ambas as velocidades a serem atuadas no
+agente. Use a classe `EvaluationAgent` para avaliar seu escore.
+
+### Verificando a robustez
+
+Com o modelo pronto, mude a seguinte linha do construtor da classe `EvaluationAgent`
+
+```python
+super().__init__(environment, randomize = False)
+```
+
+por
+
+```python
+super().__init__(environment, randomize = True)
+```
+
+Agora, execute novamente o seu modelo. Note que o modelo que funcionava anteriormente mal consegue
+seguir uma reta. Isso acontece pois sua rede estava correlacionando a cor da grama com as
+velocidades a serem seguidas. Porém, no nosso caso, a cor da grama é irrelevante para as ações do
+agente.
+
+Faça a mesma mudança no construtor da classe `DataAgent` e colete mais dados do ambiente desta vez
+aleatorizado. Concatene os dados coletados antes da aleatorização com os coletados agora e retreine
+o modelo. Avalie novamente em ambas as situações aleatorizadas (`randomize = True`) e normais
+(`randomize = False`).
---
+++ regression-cnn/agent.py
@@ -0,0 +1,201 @@
+# MAC0318 Intro to Robotics
+# Please fill-in the fields below with your info
+#
+# Name:
+# NUSP:
+#
+# ---
+#
+# Assignment 9 - End-to-end lane following with convolutional neural networks
+#
+# Don't forget to run this file from the Duckievillage root directory path (example):
+#   cd ~/MAC0318/duckievillage
+#   conda activate duckietown
+#   python3 assignments/regression-cnn/agent.py
+#
+# Submission instructions:
+#  0. Add your name and USP number to the file header above.
+#  1. Make sure that any last change hasn't broken your code. If the code crashes without running you'll get a 0.
+#  2. Submit this file via e-disciplinas.
+
+import sys
+import pyglet
+import numpy as np
+import math
+import random
+from pyglet.window import key
+from duckievillage import create_env, FRONT_VIEW_MODE
+import cv2
+import tensorflow
+
+# Uncomment the following line if you get an error with the parallel library
+# accusing multiple instantiations
+#import os
+#os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+# Basic agent.
+class Agent:
+    def __init__(self, env, randomize: bool = False):
+        self.env = env
+        self.radius = 0.0318 # R
+        self.baseline = env.unwrapped.wheel_dist/2
+        self.motor_gain = 0.68*0.0784739898632288
+        self.motor_trim = 0.0007500911693361842
+
+        if randomize:
+            f = [self.env.add_static_duckie, self.env.add_static_big_duckie, self.env.add_cone]
+            k = self.env.road_tile_size/2
+            s = (-k/2, k/2)
+            r = lambda: random.random()*random.choice(s)
+            for i, t in enumerate(self.env.grid):
+                if (t["kind"] == "floor") and (random.random() > 0.5):
+                    x, y = t["coords"]
+                    random.choice(f)(x*self.env.road_tile_size+k+r(), y*self.env.road_tile_size+k+r())
+
+            self.env.domain_rand = True
+            self.env.color_sky = [random.random(), random.random(), random.random()]
+            self.env.color_ground = (random.random(), random.random(), random.random())
+            self.env.random_reset()
+
+        key_handler = key.KeyStateHandler()
+        env.unwrapped.window.push_handlers(key_handler)
+        self.key_handler = key_handler
+
+    def get_pwm_control(self, v: float, w: float)-> (float, float):
+        ''' Takes velocity v and angle w and returns left and right power to motors.'''
+        V_l = (self.motor_gain - self.motor_trim)*(v-w*self.baseline)/self.radius
+        V_r = (self.motor_gain + self.motor_trim)*(v+w*self.baseline)/self.radius
+        return V_l, V_r
+
+    def preprocess(self) -> float:
+        pass
+
+    def send_commands(self, dt):
+        pass
+
+# Agent for data collection.
+class DataAgent(Agent):
+    def __init__(self, environment):
+        ''' Initializes agent '''
+        super().__init__(environment, randomize = False)
+
+        self.C = 6
+
+        self.images = []
+        self.labels = []
+
+    def preprocess(self) -> float:
+        '''Returns the metric to be used as signal for the PID controller.'''
+        d, alpha = self.env.lf_target()
+        return self.C*d+alpha
+
+    def send_commands(self, dt):
+        ''' Agent control loop '''
+        pwm_left, pwm_right = 0, 0
+
+        t = self.preprocess()
+        # Paste your PID controller here.
+        velocity = 0.2
+        rotation = -3.5*t
+
+        self.images.append(cv2.resize(self.env.front(), (80, 60)))
+        self.labels.append((velocity, rotation))
+
+        pwm_left, pwm_right = self.get_pwm_control(velocity, rotation)
+        self.env.step(pwm_left, pwm_right)
+        self.env.render()
+
+# Evaluation agent.
+class EvaluationAgent(Agent):
+    def __init__(self, environment):
+        ''' Initializes agent '''
+        super().__init__(environment, randomize = False)
+        self.pose_estimator = EvaluationAgent.load_regression_model("assignments/regression-cnn/cnn_lane_pos_estimation.h5")
+        self.score = 0
+
+    @staticmethod
+    def load_regression_model(filepath: str) -> tensorflow.keras.Model:
+        ''' Loads a Tensorflow model. '''
+        model = tensorflow.keras.models.load_model(filepath)
+        # If you get an error (most likely due to loading a model saved in a newer version of TensorFlow, try using the line below instead)
+        # model = tensorflow.keras.models.load_model(filepath, compile=False)
+        model.summary()
+        return model
+
+    def preprocess(self) -> (float, float):
+        '''Returns the metric to be used as signal for the PID controller.'''
+        I = cv2.resize(self.env.front(), (80, 60))/255
+        v, w = self.pose_estimator.predict(I.reshape((-1, 60, 80, 3)))[0]
+        return v, w
+
+    def send_commands(self, dt):
+        ''' Agent control loop '''
+        pwm_left, pwm_right = 0, 0
+
+        velocity, rotation = self.preprocess()
+
+        pwm_left, pwm_right = self.get_pwm_control(velocity, rotation)
+        _, r, _, _ = self.env.step(pwm_left, pwm_right)
+        self.score += (r-self.score)/self.env.step_count
+        self.env.render(text = f", score: {self.score:.3f}")
+
+def main():
+    print("MAC0318 - Assignment 9")
+    env = create_env(
+        raw_motor_input = True,
+        noisy = True,
+        mu_l = 0.007123895,
+        mu_r = -0.000523123,
+        std_l = 1e-7,
+        std_r = 1e-7,
+        seed = 101,
+        map_name = './maps/loop_empty.yaml',
+        draw_curve = False,
+        draw_bbox = False,
+        domain_rand = False,
+        user_tile_start = (0, 0),
+        distortion = False,
+        top_down = False,
+        cam_height = 10,
+        is_external_map = True,
+        randomize_maps_on_reset = False,
+        # video_path = "/tmp/regression-cnn.mp4",
+    )
+
+    env.set_view(FRONT_VIEW_MODE)
+    env.reset()
+    env.render() # show visualization
+
+    @env.unwrapped.window.event
+    def on_key_press(symbol, modifiers):
+        nonlocal agent
+        if symbol == key.ESCAPE: # exit simulation
+            # Saves dataset to /tmp.
+            if isinstance(agent, DataAgent):
+                np.save("/tmp/images.npy", agent.images, allow_pickle = True)
+                np.save("/tmp/labels.npy", agent.labels, allow_pickle = True)
+            env.close()
+            sys.exit(0)
+        elif (symbol == key.E) or (symbol == key.D):
+            # Change to evaluation or data collecting agent.
+            pyglet.clock.unschedule(agent.send_commands)
+            if (symbol == key.E) and (key.E not in agents): agents[key.E] = EvaluationAgent(env)
+            agent = agents[symbol]
+            pyglet.clock.schedule_interval(agent.send_commands, 1.0 / env.unwrapped.frame_rate)
+
+        env.render() # show image to user
+
+    # Instantiate agent
+    agents = {key.D: DataAgent(env)}
+    initial = key.E if (len(sys.argv) > 1) and (sys.argv[1] == 'eval') else key.D
+    if initial == key.E: agents[key.E] = EvaluationAgent(env)
+    agent = agents[initial]
+    # Call send_commands function from periodically (to simulate processing latency)
+    pyglet.clock.schedule_interval(agent.send_commands, 1.0 / env.unwrapped.frame_rate)
+    # Now run simulation forever (or until ESC is pressed)
+    pyglet.app.run()
+    # When it's done, close environment and exit program
+    env.close()
+
+if __name__ == '__main__':
+    main()
